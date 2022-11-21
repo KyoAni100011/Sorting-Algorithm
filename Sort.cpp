a0f8e1a9f1c012aeac6f030390978f33965f0ff0
Aplus59 <94693152+Aplus59@users.noreply.github.com>
2022-11-21 12:22:24 +0700

    Update Sort.cpp
    add ShellSort, Radix, Counting

--- Sort.cpp
+++ Sort.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <algorithm>
-
+#include <vector>
 using namespace std;
 
 void selectionSort(int a[], int n)
@@ -112,7 +112,24 @@ void radixSort(int a[], int n)
         exp *= 10;
     }
 }
-
+int shellSort(int arr[], int n)
+{
+    for (int gap = n/2; gap > 0; gap /= 2)
+    {
+        
+        for (int i = gap; i < n; i += 1)
+        {
+            int temp = arr[i];
+            int j;           
+            for (j = i; j >= gap && arr[j - gap] > temp; j -= gap)
+			{
+                arr[j] = arr[j - gap];
+			}
+            arr[j] = temp;
+        }
+    }
+    return 0;
+}
 int main()
 {
 	return 0;
