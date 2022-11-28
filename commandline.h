ce669ee55be6295fa3a68886c5a24685f391c4f9
mthng05 <90410672+mthng05@users.noreply.github.com>
2022-11-28 15:35:00 +0700

    Update commandline.h

--- commandline.h
+++ commandline.h
@@ -1,17 +1,10 @@
-<<<<<<< HEAD
 #pragma once
-=======
->>>>>>> ec5a860ca52402116f8f8addbbb5ddf33642d378
 #include <iostream>
 #include <string>
 #include <fstream>
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
-<<<<<<< HEAD
-=======
-#include <cstring>
->>>>>>> ec5a860ca52402116f8f8addbbb5ddf33642d378
 #include <algorithm>
 #include "sort.h"
 #include "sortCompare.h"
@@ -22,11 +15,7 @@ string dataOrder(int *, int, string);
 void createTempArray(int *, int *, int);
 void createFile(string);
 void readFile(int *&, int &, string);
-<<<<<<< HEAD
 void writeFile(string, int *, int, string);
-=======
-void writeFile(int *, int, string);
->>>>>>> ec5a860ca52402116f8f8addbbb5ddf33642d378
 
 void commandline1(int argc, char *argv[]);
 void commandline2(int argc, char *argv[]);
